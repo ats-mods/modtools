65a4de7f15980ee6763fdcb07da6c3530f2699e1
ats-mods <matty_119@hotmail.com>
2023-12-12 08:14:57 +0100

    the content

--- Plugin.cs
+++ Plugin.cs
@@ -1,24 +1,37 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using Eremite.Controller;
-using Eremite.Model;
-using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 using HarmonyLib;
-using UnityEngine.InputSystem;
 using Eremite.Model.Configs;
+using Eremite.Services;
+using Eremite.Tools.Runtime;
 
-namespace BubbleStormTweaks
+namespace ModdingTools
 {
     [BepInPlugin("AtsModdingTools", "AtsModdingTools", "1.0")]
     public class Plugin : BaseUnityPlugin
     {
+        private static Plugin Instance;
+        private KeyboardShortcut dumpKeyBind;
+        private KeyboardShortcut ffKeyBind;
+        public static void Log(object obj) => Instance.Logger.LogInfo(obj);
+
         private Harmony harmony;
         private void Awake()
         {
+            Instance = this;
+            Logger.LogInfo("modding tools is loaded!");
             harmony = Harmony.CreateAndPatchAll(typeof(Plugin));
+            dumpKeyBind = new(KeyCode.F1, KeyCode.LeftControl, KeyCode.LeftAlt);
+            ffKeyBind = new(KeyCode.N, KeyCode.LeftControl);
         }
+
+        private void Update(){
+            if (dumpKeyBind.IsDown()) PrintInternalGameData();
+            if (ffKeyBind.IsDown()) TimeRuntimeTools.SetNextSeason();
+        }
+
 
         [HarmonyPatch(typeof(MainController), nameof(MainController.InitBuildData))]
         [HarmonyPostfix]
@@ -28,6 +41,22 @@ namespace BubbleStormTweaks
             build.isExperimental = true;
             build.skipIntro = true;
             build.skipTutorial = true;
+        }
+
+        private void PrintInternalGameData()
+        {   
+            Instance.Logger.LogInfo("=== PRINTING ALL PERKS ===");
+            foreach (var effect in Serviceable.Settings.effects){
+                Instance.Logger.LogInfo($"{effect.Name} || (\"{effect.DisplayName}\"): uses icon name {effect.GetIcon()?.name}");
+            }
+            Log("=== PRINTING SIMPLE SEASONAL EFFECTS ===");
+            foreach (var effect in Serviceable.Settings.simpleSeasonalEffects){
+                Log($"{effect.Name} || (\"{effect.DisplayName}\"): uses icon name {effect.Icon.name}");
+            }
+            Log("=== PRINTING CONDITIONAL SEASONAL EFFECTS ===");
+            foreach (var effect in Serviceable.Settings.conditionalSeasonalEffects){
+                Log($"{effect.Name} || (\"{effect.DisplayName}\"): uses icon name {effect.Icon.name}");
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,22 @@
+# Against The Storm Modding Tools
+
+This mod activates Experimental Mode. 
+Great if you run on GOG version or can't be bothered to flip the switch in Steam settings.
+Useful because you have access to the console (use the **`** key) and because you'll use separate saves from your 'main' game so you don't mess up your progress just because you test mods.
+
+Furthermore there's two hotkeys that might be useful while developing:
+* **Ctrl+N** instantly forwards to the next season (equivalent to `season.next`)
+* **Ctrl+Alt+F1** to dump some info about all Perks (called effects, includes cornerstones and trader/ruin rewards) to log (default log location is likely `C:\Users\YOURNAME\AppData\LocalLow\Eremite Games\Against the Storm`). Information is in the form `name || ("Display Name") uses icon ICON_NAME"`
+
+
+## Installation
+
+1. Download BepInEx 5: https://github.com/BepInEx/BepInEx/releases/download/v5.4.22/BepInEx_x64_5.4.22.0.zip
+2. Extract into your Against the Storm game directory
+3. Grab the latest release of this mod from https://github.com/ats-mods/modtools/releases/latest/
+4. Extract into the same Against the Storm game directory.
+5. Run the game
+
+## Disclaimer
+
+Not associated with Eremite games. Eremite Games can't offer assistance with modded games and newer game versions may or may not break your mods. You install mods at your own risk. You may want to backup your save files from `%userprofile%\appdata\locallow\Eremite Games` prior to installing this mod.
